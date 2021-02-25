9f9b1352bd2c563bb8b86493cc66f229ec601885
ricardo <ricklimadealmeida@gmail.com>
2021-02-25 02:34:14 -0300

    02 - JS and CSS Clock (done)

--- 02 - JS and CSS Clock/index.js
+++ 02 - JS and CSS Clock/index.js
@@ -1,7 +1,9 @@
 const hourHand = document.querySelector('.hour-hand')
 const minHand = document.querySelector('.min-hand')
 const secHand = document.querySelector('.second-hand')
+const allHands = document.querySelectorAll('.hand')
 function setDate() {
+
     const now = new Date()
     const hours = (now.getHours() / 12 * 360) + 90
     hourHand.style.transform = `rotate(${hours}deg)`
@@ -9,7 +11,18 @@ function setDate() {
     minHand.style.transform = `rotate(${mins}deg)`
     const seconds = (now.getSeconds() / 60 * 360) + 90
     secHand.style.transform = `rotate(${seconds}deg)`
-
+    if (hours === 90 || mins === 90 || seconds === 90) {
+        allHands.forEach(hand => hand.style.transition = 'none') // remove the glitch when the degree reach 360
+    } else {
+        allHands.forEach(hand => hand.style.transition = '') // reset the style to the css sheet default
+    }
 }
 setDate()
 setInterval(setDate, 1000)
+
+// const allHands = document.querySelectorAll('.hand')
+// if(secondsDegrees === 90) {
+//     allHands.forEach(hand => hand.style.transition = 'none')
+// } else {
+//     allHands.forEach(hand => hand.style.transition = '')
+// }
